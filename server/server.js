b330dc773ef2b3b5c2bf7e7e6d4f3d4599f2ae42
Johnson Che <Johnson.Che01@gmail.com>
2022-02-14 23:34:30 -0500

    add postgres db endpoints

--- server/server.js
+++ server/server.js
@@ -2,57 +2,73 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-
+const accountController = require('./controllers/accountControllers');
+const applicationController = require('./controllers/applicationControllers');
+const db = require('./database');
 
 /* handle parsing request body
-*/
+ */
 app.use(express.json());
 
 /**
-* handle requests for static files
-*/
+ * handle requests for static files
+ */
+app.use(express.urlencoded({ extended: true }));
 
 /**
-* define route handlers
-*/
+ * define route handlers
+ */
 
+app.post(
+  '/signup',
+  accountController.hash,
+  accountController.newAccount,
+  (req, res) => {
+    res.status(201).json(res.locals.newUser);
+  }
+);
+
+app.post(
+  '/login',
+  accountController.getAccount,
+  accountController.verifyAccount,
+  (req, res) => {
+    res.status(201).json(res.locals.verified);
+  }
+);
 
 // route handler to respond with main app
 app.get('/', (req, res) => {
- res.status(200).json('Welcome to Jobplicity Backend!');
+  res.status(200).json('Welcome to Jobplicity Backend!');
 });
-
 
 // catch-all route handler for any requests to an unknown route
 app.use('*', (req, res) => {
- res.sendStatus(404);
+  res.sendStatus(404);
 });
 
 /**
-* configire express global error handler
-* @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
-*/
-app.use( (err, req, res, next) => {
+ * configire express global error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+app.use((err, req, res, next) => {
+  // defaultErr object
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
 
- // defaultErr object
- const defaultErr = {
-   log: 'Express error handler caught unknown middleware error',
-   status: 400,
-   message: { err: 'An error occurred' }, 
- };
-
- const errorObj = Object.assign(defaultErr, err);
- console.log(errorObj.log);
- return res.status(errorObj.status).send(errorObj.message);
-
-
+  const errorObj = Object.assign(defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).send(errorObj.message);
 });
 
 /**
-* start server
-*/
+ * start server
+ */
 app.listen(PORT, () => {
- console.log(`Server listening on port: ${PORT}`);
+  console.log(`Server listening on port: ${PORT}`);
 });
 
 module.exports = app;
