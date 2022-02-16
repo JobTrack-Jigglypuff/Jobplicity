4eeaf28ee20c0cb3a45a033b99a4985a4aa918b5
Steven Du <93756457+stebed@users.noreply.github.com>
2022-02-15 22:33:48 -0500

    [Frontend] Update Login/Signup Form validations (#16)
    * [Frontend] Working on LandingPage & Fixed Webpack
    
    * [Frontend] Setup Redux Toolkit
    
    * [Frontend] Fix Webpack issue
    
    * [Frontend] Update Login/Signup form error handling

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,15 +1,55 @@
 import * as React from 'react';
-import '../styles/Login.scss';
-import { useState } from 'react';
-import TextField from './TextField';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 import { useAppDispatch, useAppSelector } from '../Redux/hooks';
+
+// import useAxios from '../utils/useAxios';
+import TextField from './TextField';
+import { LoginForm } from '../../interfaces';
 import { setSignup } from '../Redux/slice/signupSlice';
 
 const Login = () => {
   const [error, useError] = useState(false);
+  const [username, useUsername] = useState('');
+  const [password, usePassword] = useState('');
+  // const [data, useData] = useState({username: '', password: ''});
 
   const signupState = useAppSelector((state) => state.signup.isSignup);
   const dispatch = useAppDispatch();
+
+  let navigate = useNavigate();
+
+  const handleLogin = () => {
+    const body = { username, password };
+    // useData(body);
+    axios
+      .post<LoginForm>('http://localhost:3000/login', body)
+      .then((data: AxiosResponse<any>) => {
+        console.log(data);
+        if (data.status === 201) navigate('/home', { replace: true });
+      })
+      .catch((err: AxiosError) => {
+        useError(true);
+      });
+  };
+
+  // const { response, err } = useAxios({
+  //   method: 'post',
+  //   url: 'http://localhost:3000/login',
+  //   body: data,
+  // });
+
+  // useEffect(() => {
+  //   console.log(response, err);
+  //   if (response !== null) {
+  //     if (response.status === 201) {
+  //       navigate('/home', { replace: true });
+  //     }
+  //   } else if (err === '') {
+  //     useError(true);
+  //   };
+  // }, [response]);
 
   return (
     <>
@@ -17,20 +57,24 @@ const Login = () => {
         <h1>Welcome to Jobplicity!</h1>
 
         <TextField
+          id=''
           error={error}
           label='Username'
           helperText=''
           password={false}
+          onChange={(e) => useUsername(e.target.value)}
         />
         <TextField
+          id=''
           error={error}
           label='Password'
           helperText='Incorrect Username/Password'
           password={true}
+          onChange={(e) => usePassword(e.target.value)}
         />
 
         <div className='button-container'>
-          <div className='login-button'>
+          <div className='login-button' onClick={handleLogin}>
             <a>Login</a>
           </div>
           <div
