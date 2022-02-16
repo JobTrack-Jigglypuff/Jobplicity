f15bfa76df8198bb2ba0db52e05ad08340577738
tinhtarkhin <tinhtarkhin@gmail.com>
2022-02-16 15:47:45 -0500

    merge

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
-import { BrowserRouter as Router, useNavigate} from "react-router-dom";
 import '../styles/LogOut.scss';
+import SideBar from './SideBar'
+import {Link} from 'react-router-dom';
+import '../styles/contact.scss';
+// import logo from '../asset/contact_logo.jpg';
 
 const Contact = () => {
-    const navigate = useNavigate();
-    const handleClick = () => {
-      setTimeout(()=>{navigate('/')}, 1000);
-    }
+    
+
     return (
-        <div>
-            <button className='signout-button' type="button" onClick={handleClick}>
-                GoBack
-            </button>
-        </div>
-    );
+        <>
+            <div className='contact-container'>
+                {/* <img src={logo}/> */}
+                <h1>Contact Us</h1>
+                <p>Join us on Github, or contact us below. </p>
+                <Link className='goback' to={'/home'}>
+                    <button type="button">GoBack</button>
+                </Link>
+            </div>
+            
+        </>
+      );
 };
   
   export default Contact;
